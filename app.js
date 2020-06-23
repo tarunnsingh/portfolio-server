03a98b70344436b5946382f514142a83d5214986
Tarun Singh LNMIIT <thetseffect@gmail.com>
2020-06-23 16:27:22 +0530

    Added CORS

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const morgan = require("morgan");
 const authRouter = require("./routes/auth");
 const messageRouter = require("./routes/message.js");
 const dotenv = require("dotenv");
+const cors = require("cors");
 const PORT = process.env.PORT || 5000;
 dotenv.config();
 
@@ -11,6 +12,7 @@ const app = express();
 
 app.use(express.json());
 app.use(morgan("tiny"));
+app.use(cors());
 
 mongoose.connect(
   process.env.MONGO_DB_URI,
